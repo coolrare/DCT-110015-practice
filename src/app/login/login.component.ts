db22fac163b8b50cd6b9ba956f914c5e6ea88b9b
Mike Huang <wellwind@mail2000.com.tw>
2021-09-04 14:19:23 +0800

    任務 04：在程式中動態切換路由

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,68 @@
+import { LoginService } from './../login.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { catchError, filter } from 'rxjs/operators';
+import { of, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  user = {
+    email: '',
+    password: '',
+  };
 
-  constructor() { }
+  constructor(private router: Router, private loginService: LoginService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login() {
+    // this.loginService.login(this.user).subscribe(result => {
+    //   localStorage.setItem('token', result.user.token);
+    //   this.router.navigateByUrl('/');
+    // });
+
+    // this.loginService.login(this.user).subscribe({
+    //   next: result => {
+    //     localStorage.setItem('token', result.user.token);
+    //     this.router.navigateByUrl('/');
+    //   },
+    //   error: (error: HttpErrorResponse) => {
+    //     console.log(error);
+    //     alert(error.error.body[0]);
+    //   },
+    //   complete: () => {
+    //     console.log('complete');
+    //   }
+    // });
+
+    this.loginService
+      .login(this.user)
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          console.log(error);
+          alert(error.error.body[0]);
+          return of({ user: { token: '' } });
+          // return throwError(error);
+        }),
+        filter((result) => !!result.user.token)
+      )
+      .subscribe({
+        next: (result) => {
+          console.log(result);
+          localStorage.setItem('token', result.user.token);
+          this.router.navigateByUrl('/');
+        },
+        error: (error) => {
+          console.log(error);
+        },
+        complete: () => {
+          console.log('complete');
+        },
+      });
   }
-
 }
